b3e4220b99d683d0cbcd6b92a57545089ffc0e4d
alcheny <alan.chen0219@gmail.com>
2025-04-20 17:26:06 -0400

    Added edit distance function

--- backend/similarity.py
+++ backend/similarity.py
@@ -53,3 +53,37 @@ def similar_movies(movie:int, word_occurence_matrix:np.ndarray):
         np.sum(np.minimum(word_occurence_matrix[movie], word_occurence_matrix[other_movie]))/\
         np.sum(np.maximum(word_occurence_matrix[movie], word_occurence_matrix[other_movie])))
   return sorted(scores, key=lambda x: x[1], reverse=True)[:10]
+
+#Args: string of starting query, string of target movie, insertion-cost funciton, deletion-cost function, substitution-cost function.
+def edit_distance(query: str, movie: str, ins_cost_func: int, del_cost_func: int, sub_cost_func: int) -> int:
+    query = query.lower()
+    message = message.lower()
+    table = np.zeros((len(query)+1,len(message)+1))
+    i = 1 
+    for _ in query:
+      table[i,0] = table[i-1,0] + del_cost_func(query, i)
+      i+=1
+    i = 1
+    for _ in message:
+      table[0,i] = table[0,i-1] + ins_cost_func(message, i)
+      i+=1
+    for i in range(1,len(query)+1):
+      for j in range(1,len(message)+1):
+        table[i,j] = min(
+          table[i-1,j] + del_cost_func(query, i),
+          table[i,j-1] + ins_cost_func(message, j),
+          table[i-1,j-1] + sub_cost_func(query, message, i, j),
+        )
+    return table[len(query),len(message)]
+
+def ins_cost_func(string: str, i: int):
+  if string[i] == " ":
+    return 2
+  else:
+    return 1
+  
+def del_cost_func(string: str, i: int):
+  return 1
+
+def sub_cost_func(s1: str, s2: str, i: int, j: int):
+  return 1
